97a42ee7c7f355222459b7f1da85c4f112d86362
naydeenmonzon <naydeen.monzon@icloud.com>
2021-04-26 00:39:23 -0700

    1st commit

---
+++ static/js/show-on-scroll.js
@@ -0,0 +1,32 @@
+var scroll = window.requestAnimationFrame ||
+            function(callback){ window.setTimeout(callback, 1000/60)};
+
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop() {
+
+    elementsToShow.forEach(function (element) {
+        if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+        } else {
+        element.classList.remove('is-visible');
+        }
+    });
+    
+    scroll(loop);
+}
+
+loop();
+
+const callback = function(entries) {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("is-visible");
+    });
+  };
+  
+  const observer = new IntersectionObserver(callback);
+  
+  const targets = document.querySelectorAll(".show-on-scroll");
+  targets.forEach(function(target) {
+    observer.observe(target);
+  });
