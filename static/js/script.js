4046e569a728cca588dd0c0feb9b23937b8cb47b
naydeenmonzon <naydeen.monzon@icloud.com>
2021-05-10 00:06:04 -0700

    commit

--- static/js/script.js
+++ static/js/script.js
@@ -67,14 +67,15 @@ page.forEach((page, i) => {
 gsap.to(".worldmap path", 1,{
   fill: "#013c584d",
   stagger: {
-    each: 0.1,
-    ease: "power2",
-    from:".China"},
-    scrollTrigger: {
-      trigger:".intro",
-      start:"top center",
-      markers: false}
-})
+  each: 0.1,
+  ease: "power2",
+  from:".China"},
+  scrollTrigger: {
+    trigger:".intro",
+    start:"top center",
+    markers: false}
+  });
+
 
 // --------- letter animation --------- // 
 
@@ -117,125 +118,148 @@ gsap.to(".column-3",  {
 
 // -------------------------------- TABLE -------------------------------- //
 
-// set the dimensions and margins of the graph
-var margin = {top: 0, right: 0, bottom: 0, left: 0},
-  width = 800 - margin.left - margin.right,
-  height = 600 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-var svg = d3.select("#heatmap")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+var data = d3.csv("/static/data/gport.csv").then(function(data){
+
+  var P = Array.from(d3.rollup(data,v=>v.length, d=>d.Port))
+  P.forEach(function(Array) {Array.pop()});
+  
+  const Port = P.forEach(function(){})
+  
+  return Port
+});
+console.log(data)
+// d3.rollup(data, v => v.length, d => d.key);
 
 
-//Read the data
-d3.csv("/static/data/gport.csv", function(data) {
+
+
+//console.log(map)
+// var colorDomain = d3.extent(data, function(d){
+//   return d.value;});
+
+// var colorScale = d3.scaleLinear()
+//   .domain(colorDomain)
+//   .range(["lightblue","blue"]);
+
+// var rectangles = svg.selectAll("rect")
+//   .data(data)
+//   .enter()
+//   .append("rect");
+//   rectangles
+//   .attr("x", function(d){
+//   return d.day * 50;})
+//   .attr("y", function(d){
+//   return d.week * 50;})
+//   .attr("width", 50)
+//   .attr("height", 50).
+//   style("fill", function(d){
+//     return colorScale(d.value); 
+//   });  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // set the dimensions and margins of the graph
+// var margin = {top: 100, right: 100, bottom: 100, left: 100},
+//   width = 1600 - margin.left - margin.right,
+//   height = 800 - margin.top - margin.bottom;
+
+// // append the svg object to the body of the page
+// var svg = d3.select("#heatmap")
+// .append("svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+// .append("g")
+//   .attr("transform",
+//         "translate(" + margin.left + "," + margin.top + ")");
+
+
+// //Read the data
+// d3.csv("/static/data/gport.csv", function(data) {
   
 
-  // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-  var myGroups = ['2019-01',	'2019-02',	'2019-03',	'2019-04',	'2019-05',	'2019-06',	'2019-07',	'2019-08',	'2019-09',	'2019-10',	'2019-11',	'2019-12',	'2020-01',	'2020-02',	'2020-03',	'2020-04',	'2020-05',	'2020-06',	'2020-07',	'2020-08',	'2020-09',	'2020-10',	'2020-11',	'2020-12',	'2021-01',	'2021-02',	'2021-03']
-  var myVars = ['AK',	'AL',	'CA',	'CT',	'DC',	'DE',	'FL',	'GA',	'HI',	'IL',	'IN',	'KY',	'LA',	'MA',	'MD',	'ME',	'MI',	'MN',	'MS',	'NC',	'NH',	'NJ',	'NY',	'OH',	'OR',	'PA',	'PR',	'RI',	'SC',	'TX',	'VA',	'VI',	'WA',	'WI']
+//   // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+//   var myGroups = ['2019-01',	'2019-02',	'2019-03',	'2019-04',	'2019-05',	'2019-06',	'2019-07',	'2019-08',	'2019-09',	'2019-10',	'2019-11',	'2019-12',	'2020-01',	'2020-02',	'2020-03',	'2020-04',	'2020-05',	'2020-06',	'2020-07',	'2020-08',	'2020-09',	'2020-10',	'2020-11',	'2020-12',	'2021-01',	'2021-02',	'2021-03']
+//   var myVars = ['AK',	'AL',	'CA',	'CT',	'DC',	'DE',	'FL',	'GA',	'HI',	'IL',	'IN',	'KY',	'LA',	'MA',	'MD',	'ME',	'MI',	'MN',	'MS',	'NC',	'NH',	'NJ',	'NY',	'OH',	'OR',	'PA',	'PR',	'RI',	'SC',	'TX',	'VA',	'VI',	'WA',	'WI']
 
-  // Build X scales and axis:
-  var x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(myGroups)
-    .padding(0.05);
-  svg.append("g")
-    .style("font-size", '1em')
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSize(0))
-    .select(".domain").remove()
+//   // Build X scales and axis:
+//   var x = d3.scaleBand()
+//     .range([ 0, width ])
+//     .domain(myGroups)
+//     .padding(0.05);
+//   svg.append("g")
+//     .style("font-size", '.7em')
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.axisBottom(x).tickSize(0))
+//     .select(".domain")
 
-  // Build Y scales and axis:
-  var y = d3.scaleBand()
-    .range([ height, 0 ])
-    .domain(myVars)
-    .padding(0.05);
-  svg.append("g")
-    .style("font-size", "1em")
-    .call(d3.axisLeft(y).tickSize(0))
-    .select(".domain").remove()
 
-  // Build color scale
-  var myColor = d3.scaleSequential()
-    .interpolator(d3.interpolateYlGn(0))
-    .domain([1,800])
+//   // Build Y scales and axis:
+//   var y = d3.scaleBand()
+//     .range([ height, 0 ])
+//     .domain(myVars)
+//     .padding(0.05);
+//   svg.append("g")
+//     .style("font-size", ".7em")
+//     .call(d3.axisLeft(y).tickSize(0))
+//     .select(".domain")
 
-  // create a tooltip
-  var tooltip = d3.select("#heatmap")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "2px")
-    .style("border-radius", "5px")
-    .style("padding", "5px")
+//   // Build color scale
+//   var myColor = d3.scaleSequential()
+//     .interpolator(d3.interpolateYlGn(0))
+//     .domain([1,800])
 
-  // Three function that change the tooltip when user hover / move / leave a cell
-  var mouseover = function(d) {
-    tooltip
-      .style("opacity", 1)
-    d3.select(this)
-      .style("stroke", "black")
-      .style("opacity", 1)
-  }
-  var mousemove = function(d) {
-    tooltip
-      .html("The exact value of<br>this cell is: " + d.TEU)
-      .style("left", (d3.mouse(this)[0]+70) + "px")
-      .style("top", (d3.mouse(this)[1]) + "px")
-  }
-  var mouseleave = function(d) {
-    tooltip
-      .style("opacity", 0)
-    d3.select(this)
-      .style("stroke", "none")
-      .style("opacity", 0.8)
-  }
 
-  // add the squares
-  svg.selectAll()
-    .data(data, function(d) {return d.Date+':'+d.Port;})
-    .enter()
-    .append("rect")
-      .attr("x", function(d) { return x(d.Date) })
-      .attr("y", function(d) { return y(d.Port) })
-      .attr("rx", 4)
-      .attr("ry", 4)
-      .attr("width", x.bandwidth() )
-      .attr("height", y.bandwidth() )
-      .style("fill", function(d) { return myColor(d.TEU)} )
-      .style("stroke-width", 4)
-      .style("stroke", "none")
-      .style("opacity", 0.8)
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
-})
 
-// Add title to graph
-svg.append("text")
-        .attr("x", 0)
-        .attr("y", -50)
-        .attr("text-anchor", "left")
-        .style("font-size", "22px")
-        .text("A d3.js heatmap");
+//   // add the squares
+//   svg.selectAll()
+//     .data(data, d=>d)
+//     .enter()
+//     .append("rect")
+//       .attr("x", function(d) { return x(d => d.Date) })
+//       .attr("y", function(d) { return y(d => d.Port) })
+//       .attr("rx", 4)
+//       .attr("ry", 4)
+//       .attr("width", x.bandwidth() )
+//       .attr("height", y.bandwidth() )
+//       .style("fill", function(d) { return myColor(d.TEU)} )
+//       .style("stroke-width", 4)
+//       .style("stroke", "none")
+//       .style("opacity", 0.8)
+// });
 
-// Add subtitle to graph
-svg.append("text")
-        .attr("x", 0)
-        .attr("y", -20)
-        .attr("text-anchor", "left")
-        .style("font-size", "14px")
-        .style("fill", "grey")
-        .style("max-width", 400)
-        .text("A short description of the take-away message of this chart.");
+// // Add title to graph
+// svg.append("text")
+//         .attr("x", 0)
+//         .attr("y", -50)
+//         .attr("text-anchor", "left")
+//         .style("font-size", "22px")
+//         .text("A d3.js heatmap");
 
-        
-
+// // Add subtitle to graph
+// svg.append("text")
+//         .attr("x", 0)
+//         .attr("y", -20)
+//         .attr("text-anchor", "left")
+//         .style("font-size", "14px")
+//         .style("fill", "grey")
+//         .style("max-width", 400)
+//         .text("A short description of the take-away message of this chart.");
